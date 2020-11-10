75ad9ee92441dbd9ebd5a30d057086d1abf1dfc9
Stephen Austin Brkich <austin.brkich@gmail.com>
2020-11-09 22:02:18 -0600

    Output the addresses associated with functions when debugging

--- user/main.cpp
+++ user/main.cpp
@@ -7,6 +7,12 @@ extern const LPCWSTR LOG_FILE = L"debug.txt";
 void Run() {
 #if _DEBUG
 	new_console();
+
+	using namespace app;
+
+	#define DO_APP_FUNC(r, n, p, s) printf("%s = %p\n", #n, n)
+	#include "il2cpp-functions.h"
+	#undef DO_APP_FUNC
 #endif
 
 	Game::pAmongUsClient = &(app::AmongUsClient__TypeInfo->static_fields->Instance);
